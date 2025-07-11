d854cc8c792a492f4dcf6dd1a5de2097d9ac9cf4
arnavdewan <arnavdewan05@gmail.com>
2025-07-11 23:15:51 +0530

    Create Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <a href="http://localhost:5500/login.html" className="nav-link">
+        Get Vaccinated, Stay Protected!
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
