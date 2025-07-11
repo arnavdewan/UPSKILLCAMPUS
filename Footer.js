e543d7b1f38f65c904a0b4242b18194d999ab086
arnavdewan <arnavdewan05@gmail.com>
2025-07-11 23:14:16 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Vaccination Portal &copy; 2025 | Your Health, Our Priority</p>
+    </footer>
+  );
+}
+
+export default Footer;
